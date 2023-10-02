456b348b103a3b1a9013dba92fd50b8fc69f26c7
Georgiy Shvab <georgiy.shvab@gmail.com>
2023-10-02 17:29:06 +0300

    added layout and loader to search page

---
+++ src/app/search/Loading.tsx
@@ -0,0 +1,19 @@
+import { FC } from 'react'
+import IndicatorCardLoader from '@/components/IndicatorCard/Loader'
+
+const Loading: FC = () => {
+  return (
+    <>
+      <div className="container">
+        <h2 className="mb-1.5 md:mb-3 px-2 font-semibold">Search results for</h2>
+      </div>
+      <div className="container grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-2">
+        {new Array(9).fill(null).map((item, index) => (
+          <IndicatorCardLoader key={index} />
+        ))}
+      </div>
+    </>
+  )
+}
+
+export default Loading
