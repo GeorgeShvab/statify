de25539d7264b5f35b15772281029dfca43c70cf
Georgiy Shvab <georgiy.shvab@gmail.com>
2023-10-14 00:54:05 +0300

    more optimization improvements

--- src/services/CountryService.ts
+++ src/services/CountryService.ts
@@ -7,8 +7,8 @@ const CountryService = {
   },
 
   async getCountries({ indicator }: { indicator: string }): Promise<(Country & { values: Value[] })[]> {
-    return await prisma.$queryRaw`SELECT c.*, 
-    ARRAY(SELECT JSON_BUILD_OBJECT('id', v.id, 'country', v."countryId", 'value', v.value, 'year', v.year)
+    return await prisma.$queryRaw`SELECT c.id, c.name, 
+    ARRAY(SELECT JSON_BUILD_OBJECT('value', v.value, 'year', v.year)
      FROM "Value" v WHERE "indicatorId" = ${indicator} AND "countryId" = c."id" AND "year" <= ${new Date().getFullYear()} ORDER BY "year" ASC) 
      as values FROM "Country" c WHERE (SELECT COUNT("id") FROM "Value" WHERE "indicatorId" = ${indicator} AND "countryId" = c."id" AND "year" <= ${new Date().getFullYear()}) > 0 ORDER BY "name" ASC`
   },
