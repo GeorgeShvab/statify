79509ff32da40e9b2a1e5b947c81da8c8a4df301
Georgiy Shvab <georgiy.shvab@gmail.com>
2024-01-10 20:41:26 +0200

    Slightly changed spacings and styles on indicator page. Updated open graph images, changed url to them.

--- src/components/Chart/ManageRegions/SectionButtons.tsx
+++ src/components/Chart/ManageRegions/SectionButtons.tsx
@@ -11,14 +11,14 @@ const SectionButtons: FC<Props> = ({ section, setSection }) => {
   return (
     <div className="flex top-full py-3 px-3 gap-2 w-full">
       <Button
-        className={`text-sm flex-1 ${section === 'all' ? '!bg-neutral-50' : 'text-neutral-400'}`}
+        className={`text-sm flex-1 ${section === 'all' ? '!bg-white' : '!text-neutral-400 !bg-neutral-50'}`}
         onClick={() => setSection('all')}
         color="white"
       >
         All regions
       </Button>
       <Button
-        className={`text-sm flex-1 ${section === 'selected' ? '!bg-neutral-50' : 'text-neutral-400'}`}
+        className={`text-sm flex-1 ${section === 'selected' ? '!bg-white' : '!text-neutral-400 !bg-neutral-50'}`}
         onClick={() => setSection('selected')}
         color="white"
       >
