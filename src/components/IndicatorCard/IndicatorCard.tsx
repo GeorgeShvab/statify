8a546d0d4ac470c989bb925dfe403d893e5e4d2a
Georgiy Shvab <georgiy.shvab@gmail.com>
2024-08-12 21:21:36 +0300

    some refactoring...

--- src/components/IndicatorCard/IndicatorCard.tsx
+++ src/components/IndicatorCard/IndicatorCard.tsx
@@ -3,26 +3,38 @@ import { Indicator } from '@prisma/client'
 import Link from 'next/link'
 import { FC } from 'react'
 
-interface Props extends Pick<Indicator, 'label' | 'id' | 'description' | 'source'> {
+interface Props
+  extends Pick<Indicator, 'label' | 'id' | 'description' | 'source'> {
   countryId?: string
   countryName?: string
 }
 
-const IndicatorCard: FC<Props> = ({ label, id, description, source, countryId, countryName }) => {
+const IndicatorCard: FC<Props> = ({
+  label,
+  id,
+  description,
+  source,
+  countryId,
+  countryName
+}) => {
   return (
     <Link
       href={`/indicator/${id}${countryId ? '/' + countryId : ''}`}
-      className="px-5 py-4 rounded-lg border bg-white flex flex-col justify-between gap-5 hover:shadow transition-all"
+      className='px-5 py-4 rounded-lg border bg-white flex flex-col justify-between gap-5 hover:shadow transition-all'
     >
-      <div className="">
-        <h2 className="font-bold text-lg">
+      <div>
+        <h2 className='font-bold text-lg'>
           {countryId && countryName ? countryName + ' - ' : ''}
           {label}
         </h2>
-        {source && <p className="mt-1 text-sm text-neutral-400">Source: {source}</p>}
+        {source && (
+          <p className='mt-1 text-sm text-neutral-400'>Source: {source}</p>
+        )}
       </div>
       {description && description.trim() && (
-        <p className="text-neutral-500 text-sm">{truncateString(description, 30)}</p>
+        <p className='text-neutral-500 text-sm'>
+          {truncateString(description, 30)}
+        </p>
       )}
     </Link>
   )
