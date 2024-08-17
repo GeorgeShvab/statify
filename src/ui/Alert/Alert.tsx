fb9491af8f3e20a914d71710ddbc1bce9baea86a
Georgiy Shvab <georgiy.shvab@gmail.com>
2024-08-17 23:25:41 +0300

    Added modal Provider

--- src/ui/Alert/Alert.tsx
+++ src/ui/Alert/Alert.tsx
@@ -1,26 +1,27 @@
 'use client'
 
-import { FC, useRef } from 'react'
-import { createPortal } from 'react-dom'
+import { FC } from 'react'
 import DelayWrapper from '@/ui/Alert/DelayWrapper'
 import Transition from '@/ui/Alert/Transition'
 import { AlertProps } from '@/ui/Alert/Alert.types'
 import getAlertBackgorundColor from '@/ui/Alert/utils/getAlertBackgroundColor'
+import dynamic from 'next/dynamic'
+
+const Portal = dynamic(() => import('@/components/Portal'), { ssr: false })
 
 const Alert: FC<AlertProps> = ({ show, text, severity }) => {
-  const portalElement = useRef(document.querySelector('#portal')!)
-
   const bg = getAlertBackgorundColor(severity)
 
-  return createPortal(
-    <DelayWrapper show={show} wrapper={<Transition />}>
-      <div
-        className={`z-50 fixed top-0 left-1/2 translate-x-[-50%] rounded-lg px-4 py-2 transition-all ${bg}`}
-      >
-        <p className='text-white text-sm md:text-base text-center'>{text}</p>
-      </div>
-    </DelayWrapper>,
-    portalElement.current
+  return (
+    <Portal>
+      <DelayWrapper show={show} wrapper={<Transition />}>
+        <div
+          className={`z-50 fixed top-0 left-1/2 translate-x-[-50%] rounded-lg px-4 py-2 transition-all ${bg}`}
+        >
+          <p className='text-white text-sm md:text-base text-center'>{text}</p>
+        </div>
+      </DelayWrapper>
+    </Portal>
   )
 }
 
