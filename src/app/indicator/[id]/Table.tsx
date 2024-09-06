6805d913f5d0743cfd3b7381cfc8f5ff6cf8d65a
Heorhii Shvab <georgiy.shvab@gmail.com>
2024-09-06 16:26:15 +0300

    Fixed paddings on Indicator Section, fixed some console errors, added prettier config

--- src/app/indicator/[id]/Table.tsx
+++ src/app/indicator/[id]/Table.tsx
@@ -1,62 +1,62 @@
-'use client'
+"use client";
 
-import { CountryRowValue } from '@/types'
-import { FC } from 'react'
-import Row from '@/app/indicator/[id]/Row'
-import useTableData from '@/app/indicator/[id]/useTableData'
-import { Indicator } from '@prisma/client'
-import TableComponent from '@/ui/Table/Table'
-import TableHeaderCell from '@/ui/TableHeaderCell/TableHeaderCell'
-import TableHeaderCellWithSorting from '@/ui/TableHeaderCellWithSorting/TableHeaderCellWithSorting'
+import { CountryRowValue } from "@/types";
+import { FC } from "react";
+import Row from "@/app/indicator/[id]/Row";
+import useTableData from "@/app/indicator/[id]/useTableData";
+import { Indicator } from "@prisma/client";
+import TableComponent from "@/ui/Table/Table";
+import TableHeaderCell from "@/ui/TableHeaderCell/TableHeaderCell";
+import TableHeaderCellWithSorting from "@/ui/TableHeaderCellWithSorting/TableHeaderCellWithSorting";
 
 interface Props {
-  data: CountryRowValue[]
-  indicator: Indicator
+  data: CountryRowValue[];
+  indicator: Indicator;
 }
 
 const Table: FC<Props> = (props) => {
-  const [data, handleSort] = useTableData(props.data)
+  const [data, handleSort] = useTableData(props.data);
 
-  const handleCountrySort = () => handleSort('country')
-  const handleValueSort = () => handleSort('value')
+  const handleCountrySort = () => handleSort("country");
+  const handleValueSort = () => handleSort("value");
 
   const renderHeader = () => (
     <tr>
       <TableHeaderCellWithSorting
-        className='w-24 md:w-[350px] !font-bold'
+        className="w-24 md:w-[350px] !font-bold"
         onSortChange={handleCountrySort}
         direction={data.order}
-        isSelected={data.by === 'country'}
-        aria-label='Sort by region name'
+        isSelected={data.by === "country"}
+        aria-label="Sort by region name"
       >
         Region
       </TableHeaderCellWithSorting>
       <TableHeaderCellWithSorting
-        className='text-right w-28 md:w-64'
+        className="text-right w-28 md:w-64"
         onSortChange={handleValueSort}
         direction={data.order}
-        isSelected={data.by === 'value'}
-        buttonProps={{ className: 'justify-end' }}
-        aria-label='Sort by value'
+        isSelected={data.by === "value"}
+        buttonProps={{ className: "justify-end" }}
+        aria-label="Sort by value"
       >
         Value
       </TableHeaderCellWithSorting>
-      <TableHeaderCell className='text-center w-[100px] md:w-48 whitespace-nowrap'>
+      <TableHeaderCell className="text-center w-[100px] md:w-48 whitespace-nowrap">
         Trend
       </TableHeaderCell>
-      <TableHeaderCell className='pl-3 pr-4 md:pr-6 md:pl-6 text-right w-[5px] md:w-[75px]'>
+      <TableHeaderCell className="pl-3 pr-4 md:pr-6 md:pl-6 text-right w-[5px] md:w-[75px]">
         Year
       </TableHeaderCell>
     </tr>
-  )
+  );
 
   const renderRow = (item: CountryRowValue) => (
     <Row key={item.id} indicator={props.indicator} country={item} />
-  )
+  );
 
   return (
-    <section className='container'>
-      <div className='bg-white rounded-lg border'>
+    <section className="container mb-2 md:mb-3.5">
+      <div className="bg-white rounded-lg border">
         <TableComponent
           data={data.data}
           renderRow={renderRow}
@@ -64,7 +64,7 @@ const Table: FC<Props> = (props) => {
         />
       </div>
     </section>
-  )
-}
+  );
+};
 
-export default Table
+export default Table;
--- src/ui/TableHeaderCellWithSorting/TableHeaderCellWithSorting.tsx
+++ src/ui/TableHeaderCellWithSorting/TableHeaderCellWithSorting.tsx
@@ -1,41 +1,42 @@
-import TableSortIcon from '@/components/TableSortIcon/TableSortIcon'
-import { ReactNode, ComponentProps, FC } from 'react'
-import TableHeaderCell from '@/ui/TableHeaderCell/TableHeaderCell'
+import TableSortIcon from "@/components/TableSortIcon/TableSortIcon";
+import { ReactNode, ComponentProps, FC } from "react";
+import TableHeaderCell from "@/ui/TableHeaderCell/TableHeaderCell";
 
-interface TableHeaderCellWithSortingProps extends ComponentProps<'th'> {
-  children: ReactNode
-  direction: 'asc' | 'desc'
-  isSelected: boolean
-  buttonProps?: ComponentProps<'button'>
-  onSortChange: () => void
+interface TableHeaderCellWithSortingProps extends ComponentProps<"th"> {
+  children: ReactNode;
+  direction: "asc" | "desc";
+  isSelected: boolean;
+  buttonProps?: ComponentProps<"button">;
+  onSortChange: () => void;
 }
 
 const TableHeaderCellWithSorting: FC<TableHeaderCellWithSortingProps> = ({
   children,
-  className = '',
+  className = "",
   direction,
   isSelected,
   onSortChange,
+  buttonProps,
   ...props
 }) => {
-  const { className: buttonClassName, ...buttonProps } = props.buttonProps || {}
-
-  const buttonComponentClassName = `flex items-center gap-1.5 md:gap-3 justify-start w-full text-right ${buttonClassName || ''}`
+  const buttonComponentClassName = `flex items-center gap-1.5 md:gap-3 justify-start w-full text-right ${
+    buttonProps?.className || ""
+  }`;
 
   return (
     <TableHeaderCell className={className} {...props}>
       <button
         onClick={onSortChange}
-        className={buttonComponentClassName}
         {...buttonProps}
+        className={buttonComponentClassName}
       >
         {children}
-        <span className='w-4 h-4 flex items-center justify-center'>
+        <span className="w-4 h-4 flex items-center justify-center">
           <TableSortIcon direction={direction} isSelected={isSelected} />
         </span>
       </button>
     </TableHeaderCell>
-  )
-}
+  );
+};
 
-export default TableHeaderCellWithSorting
+export default TableHeaderCellWithSorting;
