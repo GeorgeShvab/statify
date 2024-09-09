9112dfc2360948f7a57d0928eeee8e24ab507d07
Heorhii Shvab <georgiy.shvab@gmail.com>
2024-09-09 23:24:27 +0300

    fixed dropdown

--- src/components/Animation/AnimationWrapper.tsx
+++ src/components/Animation/AnimationWrapper.tsx
@@ -1,4 +1,6 @@
-import { FC, ReactNode, useEffect, useLayoutEffect, useRef, useState } from 'react'
+import useOpenCloseAnimation from "@/hooks/useOpenCloseAnimation"
+import cn from "@/utils/cn"
+import { FC, ReactNode } from "react"
 
 interface Props {
   children: ReactNode
@@ -7,34 +9,26 @@ interface Props {
   ms?: number
 }
 
-const AnimationWrapper: FC<Props> = ({ children, open, renderHidden = false, ms = 200 }) => {
-  const [isAnimationActive, setIsAnimationActive] = useState<boolean>(false)
-  const [isMounted, setIsMounted] = useState<boolean>(false)
-
-  const time = useRef<NodeJS.Timeout>()
-
-  useLayoutEffect(() => {
-    if (open) {
-      clearTimeout(time.current)
-      setIsMounted(true)
-    } else if (!open && isMounted) {
-      time.current = setTimeout(() => {
-        setIsMounted(false)
-      }, ms)
-    }
-  }, [open])
-
-  useEffect(() => {
-    if (isMounted && open) {
-      setIsAnimationActive(true)
-    } else if (isMounted && !open) {
-      setIsAnimationActive(false)
-    }
-  }, [isMounted, open])
+const AnimationWrapper: FC<Props> = ({
+  children,
+  open,
+  renderHidden = false,
+  ms = 200,
+}) => {
+  const { isVisible, isMounted } = useOpenCloseAnimation(open, ms)
 
   if (!isMounted && !renderHidden) return null
 
-  return <div className={isAnimationActive ? 'animation-active' : 'animation-inactive'}>{children}</div>
+  return (
+    <div
+      className={cn(
+        isVisible ? "animation-active" : "animation-inactive",
+        renderHidden && !isMounted && "invisible"
+      )}
+    >
+      {children}
+    </div>
+  )
 }
 
 export default AnimationWrapper
