f3c961f9b17cec854990f3f2a785f1eabbcb09bf
Heorhii Shvab <georgiy.shvab@gmail.com>
2024-09-24 13:54:07 +0300

    Added refresh button

--- src/containers/indicators-dashboard-header/IndicatorsDashboardHeader.tsx
+++ src/containers/indicators-dashboard-header/IndicatorsDashboardHeader.tsx
@@ -1,18 +1,25 @@
 "use client"
 
+import { useRouter } from "next/navigation"
 import Button from "@/ui/button/Button"
+import IconButton from "@/ui/icon-button/IconButton"
 import PlusIcon from "@/ui/icons/PlusIcon"
+import RefreshIcon from "@/ui/icons/RefreshIcon"
 import CreateIndicatorModal from "@/containers/modals/create-indicator-modal/CreateIndicatorModal"
 import DashboardHeading from "@/components/dashboard-heading/DashboardHeading"
 import { useModal } from "@/providers/modal-provider/ModalProvider"
 import "@/containers/indicators-dashboard-header/styles.scss"
 
 const IndicatorsDashboardHeader = () => {
+  const router = useRouter()
+
   const { openModal } = useModal()
 
   const handleCreateIndicatorClick = () => {
     openModal(<CreateIndicatorModal />, { scrollable: true })
   }
+
+  const handleRefresh = () => router.refresh()
 
   return (
     <div className="dashboard-header">
@@ -28,6 +35,9 @@ const IndicatorsDashboardHeader = () => {
         <PlusIcon />
         <span>Create</span>
       </Button>
+      <IconButton onClick={handleRefresh}>
+        <RefreshIcon />
+      </IconButton>
     </div>
   )
 }
