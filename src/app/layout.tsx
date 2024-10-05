115a6198d5b4f2b3107fd1559c123dad205ce8e1
Heorhii Shvab <georgiy.shvab@gmail.com>
2024-10-05 14:19:28 +0300

    Added confirm modal (#51)

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,9 +1,10 @@
-import "@/styles/globals.css"
+import { Analytics } from "@vercel/analytics/react"
+import { PropsWithChildren } from "react"
 import { Roboto } from "next/font/google"
 import AlertProvider from "@/providers/alert-provider/AlertProvider"
+import ConfirmProvider from "@/providers/confirm-provider/ConfirmProvider"
 import ModalProvider from "@/providers/modal-provider/ModalProvider"
-import { PropsWithChildren } from "react"
-import { Analytics } from "@vercel/analytics/react"
+import "@/styles/globals.css"
 import "@/styles/global.scss"
 
 const roboto = Roboto({
@@ -15,13 +16,15 @@ export default function RootLayout({ children }: PropsWithChildren) {
   return (
     <html lang="en">
       <body className={`${roboto.className} bg-neutral-50`}>
-        <AlertProvider>
-          <ModalProvider>
-            <div id="portal" />
-            {children}
-            <Analytics />
-          </ModalProvider>
-        </AlertProvider>
+        <ConfirmProvider>
+          <AlertProvider>
+            <ModalProvider>
+              <div id="portal" />
+              {children}
+              <Analytics />
+            </ModalProvider>
+          </AlertProvider>
+        </ConfirmProvider>
       </body>
     </html>
   )
