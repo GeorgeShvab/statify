ac1db74c6ddd63b4c8a61b0eae04fd01d57f3967
Heorhii Shvab <georgiy.shvab@gmail.com>
2024-10-09 01:14:57 +0300

    Added not found view for dashboard searching (on countries and indicators pages) (#61)

---
+++ src/containers/admin-dashboard/components/admin-dashboard-not-found-view/AdminDashboardNotFoundView.tsx
@@ -0,0 +1,15 @@
+import SearchIcon from "@/ui/icons/SearchIcon"
+import "@/containers/admin-dashboard/components/admin-dashboard-not-found-view/styles.scss"
+
+const AdminDashboardNotFoundView = () => {
+  return (
+    <div className="admin-dashboard__not-found">
+      <div aria-hidden={true} className="admin-dashboard__not-found-icon">
+        <SearchIcon className="w-20 h-20" />
+      </div>
+      <p className="admin-dashboard__not-found-title">Nothing was found</p>
+    </div>
+  )
+}
+
+export default AdminDashboardNotFoundView
