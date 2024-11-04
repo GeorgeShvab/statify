85e049438ab3ecd65a963a93b0f9a9ca4eda11f5
Heorhii Shvab <georgiy.shvab@gmail.com>
2024-11-05 01:00:09 +0200

    Rewrited header to scss

--- src/layout/admin-header/AdminHeader.tsx
+++ src/layout/admin-header/AdminHeader.tsx
@@ -6,6 +6,7 @@ import { usePathname } from "next/navigation"
 import Button from "@/ui/button/Button"
 import routes from "@/constants/routes"
 import "@/layout/admin-header/styles.scss"
+import "@/layout/header/styles.scss"
 
 const AdminHeader: FC = () => {
   const path = usePathname()
@@ -15,52 +16,47 @@ const AdminHeader: FC = () => {
   const isValues = path.includes("values")
 
   return (
-    <header className="admin-header">
-      <div className="admin-header__container">
-        <Link
-          href="/"
-          className="admin-header__logo font-bold text-3xl font-serif"
-        >
-          Statify
-        </Link>
-        <nav className="admin-header__nav">
-          <ul className="admin-header__nav-list">
-            <li>
-              <Button
-                size="small"
-                className="admin-header__nav-item"
-                variant={isIndicators ? "contained" : "text"}
-                color={isIndicators ? "light" : "dark"}
-                href={routes.admin.indicators}
-              >
-                Indicators
-              </Button>
-            </li>
-            <li>
-              <Button
-                size="small"
-                className="admin-header__nav-item"
-                variant={isCountries ? "contained" : "text"}
-                color={isCountries ? "light" : "dark"}
-                href={routes.admin.countries}
-              >
-                Countries
-              </Button>
-            </li>
-            <li>
-              <Button
-                size="small"
-                className="admin-header__nav-item"
-                variant={isValues ? "contained" : "text"}
-                color={isValues ? "light" : "dark"}
-                href={routes.admin.values}
-              >
-                Values
-              </Button>
-            </li>
-          </ul>
-        </nav>
-      </div>
+    <header className="header">
+      <Link href="/" className="header__logo">
+        Statify
+      </Link>
+      <nav className="admin-header__nav">
+        <ul className="admin-header__nav-list">
+          <li>
+            <Button
+              size="small"
+              className="admin-header__nav-item"
+              variant={isIndicators ? "contained" : "text"}
+              color={isIndicators ? "light" : "dark"}
+              href={routes.admin.indicators}
+            >
+              Indicators
+            </Button>
+          </li>
+          <li>
+            <Button
+              size="small"
+              className="admin-header__nav-item"
+              variant={isCountries ? "contained" : "text"}
+              color={isCountries ? "light" : "dark"}
+              href={routes.admin.countries}
+            >
+              Countries
+            </Button>
+          </li>
+          <li>
+            <Button
+              size="small"
+              className="admin-header__nav-item"
+              variant={isValues ? "contained" : "text"}
+              color={isValues ? "light" : "dark"}
+              href={routes.admin.values}
+            >
+              Values
+            </Button>
+          </li>
+        </ul>
+      </nav>
     </header>
   )
 }
