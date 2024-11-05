8402194357f54b816fa72507b8763e136871a709
Heorhii Shvab <georgiy.shvab@gmail.com>
2024-11-05 11:49:09 +0200

    Rewrited error pages to scss (#130)

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,34 +1,21 @@
 import { FC } from "react"
 import { Metadata } from "next"
-import Link from "next/link"
 import NotFoundIcon from "@/ui/icons/NotFoundIcon"
 import Footer from "@/layout/footer/Footer"
 import Header from "@/layout/header/Header"
+import StatusPage from "@/containers/status-page/StatusPage"
 import { SERVER_ADDRESS } from "@/constants/general"
 
 const NotFoundPage: FC = () => {
   return (
     <>
       <Header />
-      <main className="bg-white">
-        <div className="container">
-          <div className="flex flex-col justify-center items-center view-height">
-            <div>
-              <div className="px-2 md:px-0">
-                <div className="flex justify-center mb-16 text-[#121212]">
-                  <NotFoundIcon />
-                </div>
-                <h1 className="text-center text-3xl md:text-4xl font-bold text-black mb-4 md:mb-6 leading-normal">
-                  Page is not found
-                </h1>
-                <div className="text-neutral-400 hover:text-neutral-800 transition-colors text-center">
-                  <Link href="/">To home page</Link>
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
-      </main>
+      <StatusPage
+        icon={<NotFoundIcon />}
+        title={"Page is not found"}
+        linkHref="/"
+        linkText="To home page"
+      />
       <Footer />
     </>
   )
---
+++ src/containers/status-page/types.ts
@@ -0,0 +1,8 @@
+import { ReactNode } from "react"
+
+export interface StatusPageProps {
+  icon: ReactNode
+  title: string
+  linkText: string
+  linkHref: string
+}
